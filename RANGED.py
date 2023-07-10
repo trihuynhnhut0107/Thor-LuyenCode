4eaa2028616d043797f8aaec7e246600e3f66c78
ITechIOI <22520783@gm.uit.edu.vn>
2023-07-10 14:48:49 +0700

    TL

---
+++ RANGED.py
@@ -0,0 +1,21 @@
+def RANGED(a, b, c, d):
+    if a >= c and b <= d:
+        return True
+    elif a <= c and b <= d and c <= b:
+        return True
+    elif a <= c and b >= d and a <= d:
+        return True
+    else:
+        return False
+def main():
+    a, b, c, d = input().split()
+    a = int(a)
+    b = int(b)
+    c = int(c) 
+    d = int(d)
+    if RANGED(a, b, c, d):
+        print("YES")
+    else:
+        print("NO")
+if __name__ == "__main__":
+    main()
